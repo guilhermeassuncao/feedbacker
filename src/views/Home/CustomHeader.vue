3c6fd547212194a9a5aaef774c51d365fc3f91d2
Guilherme Assunção <cgmatosassuncao@gmail.com>
2022-03-04 20:29:18 -0300

    Header

---
+++ src/views/Home/CustomHeader.vue
@@ -0,0 +1,70 @@
+<template>
+    <header class="header">
+        <div class="header-group">
+            <div class="flex items-center justify-between py-10">
+                <div class="w-28 lg:w-36">
+                    <img
+                        class="w-full"
+                        src="../../assets/images/logo_white.png"
+                        alt="Logo Feedbacks"
+                    />
+                </div>
+                <div class="flex">
+                    <button
+                        class="px-6 py-2 font-bold rounded-full text-white focus:outline-none"
+                    >
+                        Crie uma conta
+                    </button>
+                    <button
+                        class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none shadow-sm"
+                    >
+                        Entrar
+                    </button>
+                </div>
+            </div>
+            <div class="flex flex-col mt-28">
+                <h1 class="text-4xl font-black text-white">
+                    Tenha um feedback. <br />
+                    E faça seus clientes mais
+                    <br class="hidden lg:inline-block" />
+                    felizes!
+                </h1>
+                <p class="text-lg font-medium text-white mt-5">
+                    Receba ideias, reclamações e feedbacks com um
+                    <br class="hidden lg:inline-block" />
+                    simples widget na página.
+                </p>
+                <div class="mt-12">
+                    <button
+                        class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none shadow-sm"
+                    >
+                        Crie uma conta grátis
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="postcss" scoped>
+.header {
+    @apply bg-brand-main w-full flex justify-center;
+    height: 43.75rem;
+}
+
+.header-group {
+    @apply flex flex-col w-4/5 max-w-6xl;
+}
+
+@media (min-width: 640px) {
+    .header-group {
+        background-image: url("../../assets/images/blue_balloons.png");
+        background-size: 39.25rem;
+        background-position: 90% 100%;
+        background-repeat: no-repeat;
+    }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,19 +1,42 @@
 <template>
-    <custom-header />
+    <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
     <contact />
     <footer class="flex justify-center py-6 bg-brand-main shadow-sm">
-        <p class="text-center font-medium text-gray-100">© 2022 Feedback, All rights reserved.</p>
+        <p class="text-center font-medium text-gray-100">
+            © 2022 Feedback, All rights reserved.
+        </p>
     </footer>
 </template>
 <script>
-import CustomHeader from './CustomHeader.vue'
-import Contact from './Contact.vue'
+import CustomHeader from "./CustomHeader.vue";
+import Contact from "./Contact.vue";
+import { useRouter } from 'vue-router';
+import { onMounted } from '@vue/runtime-core';
 
 export default {
     components: {
         CustomHeader,
-        Contact
-    }
+        Contact,
+    },
+    setup() {
+        const router = useRouter();
+
+        onMounted(() => {
+            const token = window.localStorage.getItem("token");
+
+            if (token) {
+                router.push({name: "Feedbacks"});
+            }
+        });
+        function handleAccountCreate() {}
+
+        function handleLogin() {}
+
+        return {
+            handleAccountCreate,
+            handleLogin,
+        };
+    },
 };
 </script>
 <style></style>
