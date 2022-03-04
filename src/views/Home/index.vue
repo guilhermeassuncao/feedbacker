1f7cb01a81e5a3d0c0817118ea6102e3b1a56e6a
Guilherme Assunção <cgmatosassuncao@gmail.com>
2022-03-04 19:32:39 -0300

    Init

---
+++ src/views/Home/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <custom-header />
+    <contact />
+    <footer class="flex justify-center py-6 bg-brand-main shadow-sm">
+        <p class="text-center font-medium text-gray-100">© 2022 Feedback, All rights reserved.</p>
+    </footer>
+</template>
+<script>
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+export default {
+    components: {
+        CustomHeader,
+        Contact
+    }
+};
+</script>
+<style></style>
